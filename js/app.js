96f62e861dacbb4fba0cf81ade282df4a7d1b596
Lonewolf2023 <auroracloudstudios@gmail.com>
2023-09-18 04:11:56 -0400

    Complete Functionality of App

--- js/app.js
+++ js/app.js
@@ -1,9 +1,3 @@
-
-
-
-
-// or write: let minutes;
-
 const randomDay = () =>{
     const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     return daysOfWeek[Math.floor(Math.random()*7)]
@@ -13,28 +7,27 @@ const randomDay = () =>{
 
 const randomMinutes = () =>{
     const minutes = [15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210, 225, 240];
-    return minutes[Math.floor(Math.random()*16)];
+    let hours = minutes[Math.floor(Math.random()*16)]/60 + ' hrs';
+    return hours;
 }
-//* May place if statement within the randomMinutes to retrieve 'hours' string. *
 //console.log(randomMinutes()) - Test 2
 
-
-let goals = {
-    study: [{
+const goals = {
+    study: {
         code: ['Full-Stack (HTML5/CSS/JS)', 'Java', 'Python', 'C++'],
         languages: ['Spanish', 'Japanese', 'French', 'German'],
         healthScience: ['Diet + Nutrition', 'Workout Routines', 'Anatomy']
-    }],
-    exercise: [{
+    },
+    exercise: {
         physicalTherapy: ['Circuits', 'Aquatics'],
         cardio: ['Bike', 'Elliptical', 'Swim Laps'],
         bodyBuild: ['Abs', 'Chest', 'Back/Neck', 'Legs', 'Arms']
-    }],
-    meditate: [{
+    },
+    meditate: {
         spiritual: ['Read Bible', 'Reflect/Meditate'],
-        social: ['Friends Time', 'Family Time', 'Party Time'],
-        personal: ['Social Media', 'Play Video Games', 'Watch TV', 'Singing', 'Dancing', 'Art(2D/3D/Digital)']
-    }]
+        social: ['Hang Out', 'Family Time', 'Party Time'],
+        personal: ['Social Media', 'Play Video Games', 'Watch TV', 'Singing', 'Dancing', 'Art (2D/3D/Digital)']
+    }
 }
 
 const randomGoal = goal => Math.floor(Math.random()* goal);
@@ -42,28 +35,39 @@ const randomGoal = goal => Math.floor(Math.random()* goal);
 let todaysGoals = [];
 
 for (let task in goals){
-    let taskIndex = randomGoal(goals[task].length);
 
-switch(task){
-    case 'study':
-        todaysGoals.push(`Study ${goals[task][taskIndex]} - ${randomMinutes()}`);
-    break;
-    case 'exercise':
-        todaysGoals.push(`Exercise Challenge: ${goals[task][taskIndex]} - ${randomMinutes()}`);
-    break;
-    case 'meditate':
-        todaysGoals.push(`Meditate/Relax ${goals[task][taskIndex]} - ${randomMinutes()}`);
-    break;
-    default: 
-        todaysGoals.push(`Time to go for a random run!`)
-    }
+
+    switch(task){
+        case 'study':
+            for(let newStudy in goals.study){
+                let studyIndex = randomGoal(goals.study[newStudy].length);
+                todaysGoals.push(`Study: ${goals[task][newStudy][studyIndex]} - ${randomMinutes()}`)
+            }
+        break;
+        case 'exercise':
+            for (let newExercise in goals.exercise){
+                let exerIndex = randomGoal(goals.exercise[newExercise].length);
+                todaysGoals.push(`Exercise: ${goals[task][newExercise][exerIndex]} - ${randomMinutes()}`)
+            }
+        break;
+        case 'meditate':
+            for (let newMeditate in goals.meditate){
+                let medIndex = randomGoal(goals.meditate[newMeditate].length);
+                todaysGoals.push(`Meditate: ${goals[task][newMeditate][medIndex]} - ${randomMinutes()}`)
+            }
+        break;
+        default: 
+            todaysGoals.push(`Time to go for a random run!`)
+        }
+
 }
 
 const fullMessage = message =>{
     let fullSchedule = todaysGoals.join('\n')
     console.log(fullSchedule)
 }
-console.log(randomDay())
+
+console.log(`${randomDay()}'s Goal Challenges:`)
 fullMessage(todaysGoals);
 
 
